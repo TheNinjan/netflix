a4da931878cf179c3431e5855be87b4d426c813d
N100lanki <theninjan.com@gmail.com>
2023-09-05 13:41:48 +0530

    Minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Mulish } from 'next/font/google'
 const mulish = Mulish({ subsets: ['latin'],display:'swap',weight:'400' })
 
 export const metadata: Metadata = {
-  title: 'Flex',
-  description: 'Created by The Ninjan | Niranjan Solanki',
+  title: 'Netflix',
+  description: 'Created by The Ninjan | Niranjan Solanki | Netflix ',
 }
 
 export default function RootLayout({
